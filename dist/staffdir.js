2b33bba639bedd425e33cc5488176f54d6040198
Will Jones <the8bitsquid@gmail.com>
2015-12-04 17:15:44 -0600

    Live Build: implementing lazyloading images and new hightlight filter

--- dist/staffdir.js
+++ dist/staffdir.js
@@ -575,7 +575,7 @@ angular.module('staffdir', ['ualib.staffdir']);
         return {
             restrict: 'AC',
             templateUrl: 'staff-card/staff-card-list.tpl.html',
-            controller: function($scope){
+            controller: ['$scope', function($scope){
                 $scope.staffdir = {};
 
                 StaffFactory.directory().get()
@@ -586,7 +586,7 @@ angular.module('staffdir', ['ualib.staffdir']);
                     }, function(){
                         console.log('Staffdir Error -- Come on, put in proper error handling already');
                     });
-            }
+            }]
         };
     }])
 
@@ -771,7 +771,7 @@ angular.module('staffdir', ['ualib.staffdir']);
                 login: '@email'
             },
             templateUrl: 'staff-profile/staff-profile.tpl.html',
-            controller: function($scope){
+            controller: ['$scope', function($scope){
                 $scope.userProfile = {};
 
                 //console.log("Login: " + $scope.login);
@@ -817,7 +817,7 @@ angular.module('staffdir', ['ualib.staffdir']);
                     }, function(data){
                         console.log('Error: cold not get profile! ' + data);
                     });
-            }
+            }]
         };
     }]);
 
